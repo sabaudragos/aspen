1cab11dd9d62419e6752abf6016621d5e9c3b1e6
dragos <sabaudragos@yahoo.com>
2017-04-13 07:58:24 +0300

    - switched to try with resources for jgit related operations

--- src/main/java/xyz/codeark/service/GitServiceImpl.java
+++ src/main/java/xyz/codeark/service/GitServiceImpl.java
@@ -3,9 +3,8 @@ package xyz.codeark.service;
 import lombok.extern.slf4j.Slf4j;
 import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.api.PullResult;
-import org.eclipse.jgit.api.RebaseResult;
 import org.eclipse.jgit.api.Status;
-import org.eclipse.jgit.api.errors.GitAPIException;
+import org.eclipse.jgit.api.errors.*;
 import org.eclipse.jgit.lib.BranchTrackingStatus;
 import org.eclipse.jgit.lib.ObjectId;
 import org.eclipse.jgit.lib.Repository;
@@ -29,32 +28,34 @@ public class GitServiceImpl implements GitService {
     public GitRepository pull(GitRepository gitRepository, Boolean userRebase) {
         log.info("Updating repository: {}", gitRepository.getName());
 
-        Git git = getGitInstance(gitRepository.getPath());
         PullResult pullResult = null;
+        try (Repository repository = getJGitRepository(gitRepository.getPath())){
+            try (Git git = new Git(repository)) {
+                Status status = git.status().call();
+                RevCommit stash = null;
 
-        try {
-            Status status = git.status().call();
-            RevCommit stash = null;
+                if (status.hasUncommittedChanges()) {
+                    stash = createStash(git);
+                }
 
-            if (status.hasUncommittedChanges()) {
-                stash = createStash(git);
+                pullResult = git.pull().setRebase(userRebase).call();
+
+                if (stash != null) {
+                    log.info("Applying stash");
+                    ObjectId appliedStashId = git.stashApply().setStashRef(stash.getName()).call();
+                    log.info("Stash with ID {} was applied successfully", appliedStashId);
+                }
+            } catch (GitAPIException e) {
+                log.error("Error while updating the repository", e);
+                throw new AspenRestException(RestConstants.GIT_PULL_FAILED, Response.Status.ACCEPTED);
             }
-
-            pullResult = git.pull().setRebase(userRebase).call();
-
-            if (stash != null) {
-                log.info("Applying stash");
-                ObjectId appliedStashId = git.stashApply().setStashRef(stash.getName()).call();
-                log.info("Stash with ID {} was applied successfully", appliedStashId);
-            }
-        } catch (GitAPIException e) {
-            log.error("Error while updating the repository", e);
-            throw new AspenRestException(RestConstants.GIT_PULL_FAILED, Response.Status.ACCEPTED);
+        } catch (IOException e) {
+            log.error("Error while building a git instance", e);
+            throw new AspenRestException(RestConstants.ERROR_BUILDING_GIT_INSTANCE, Response.Status.ACCEPTED);
         }
 
         // should both the rebase and fetch results be treated?
-
-        pullResult.getRebaseResult().getStatus().equals(RebaseResult.Status.UP_TO_DATE);
+//        pullResult.getRebaseResult().getStatus().equals(RebaseResult.Status.UP_TO_DATE);
 
         if (pullResult.getRebaseResult().getStatus().isSuccessful()) {
             gitRepository.setStatus(RestConstants.GIT_PULL_SUCCESS);
@@ -85,42 +86,44 @@ public class GitServiceImpl implements GitService {
     public GitRepository isUpToDate(String repositoryPath) {
         log.info("Checking if repository \'{}\' is up to date", repositoryPath);
 
-        Git git = getGitInstance(repositoryPath);
-
         GitRepository gitRepository = new GitRepository();
         gitRepository.setPath(repositoryPath);
         gitRepository.setName(repositoryPath.substring(repositoryPath.lastIndexOf('/') + 1));
 
-        try {
-            BranchTrackingStatus branchTrackingStatus =
-                    BranchTrackingStatus.of(git.getRepository(), git.getRepository().getBranch());
+        try (Repository repository = getJGitRepository(repositoryPath)){
+            try (Git git = new Git(repository)) {
+                BranchTrackingStatus branchTrackingStatus =
+                        BranchTrackingStatus.of(git.getRepository(), git.getRepository().getFullBranch());
 
-            if (branchTrackingStatus == null) {
-                gitRepository.setStatus(RestConstants.GIT_NO_REMOTE_TRACKING_OF_BRANCH);
-                log.debug(RestConstants.GIT_NO_REMOTE_TRACKING_OF_BRANCH);
+                if (branchTrackingStatus == null) {
+                    gitRepository.setStatus(RestConstants.GIT_NO_REMOTE_TRACKING_OF_BRANCH);
+                    log.debug(RestConstants.GIT_NO_REMOTE_TRACKING_OF_BRANCH);
 
-                return gitRepository;
+                    return gitRepository;
+                }
+
+                if (branchTrackingStatus.getBehindCount() > 0){
+                    // local branch is outdated, is behind origin by branchTrackingStatus.getBehindCount()
+                    gitRepository.setStatus(RestConstants.GIT_REPOSITORY_IS_BEHIND_OF_ORIGIN);
+                    log.debug(RestConstants.GIT_REPOSITORY_IS_BEHIND_OF_ORIGIN);
+
+                    return gitRepository;
+                }
+
+                if (branchTrackingStatus.getBehindCount() > 0){
+                    // local branch NOT outdated, is ahead origin by branchTrackingStatus.getBehindCount()
+                    gitRepository.setStatus(RestConstants.GIT_REPOSITORY_IS_AHEAD_OF_ORIGIN);
+                    log.debug(RestConstants.GIT_REPOSITORY_IS_AHEAD_OF_ORIGIN);
+
+                    return gitRepository;
+                }
+            } catch (IOException e) {
+                log.error("Error while checking the status of {}", gitRepository.getName());
+                throw new AspenRestException(RestConstants.ERROR_WHILE_CHECKING_BRANCH_STATUS, Response.Status.ACCEPTED);
             }
-
-            if (branchTrackingStatus.getBehindCount() > 0){
-                // local branch is outdated, is behind origin by branchTrackingStatus.getBehindCount()
-                gitRepository.setStatus(RestConstants.GIT_REPOSITORY_IS_BEHIND_OF_ORIGIN);
-                log.debug(RestConstants.GIT_REPOSITORY_IS_BEHIND_OF_ORIGIN);
-
-                return gitRepository;
-            }
-
-            if (branchTrackingStatus.getBehindCount() > 0){
-                // local branch NOT outdated, is ahead origin by branchTrackingStatus.getBehindCount()
-                gitRepository.setStatus(RestConstants.GIT_REPOSITORY_IS_AHEAD_OF_ORIGIN);
-                log.debug(RestConstants.GIT_REPOSITORY_IS_AHEAD_OF_ORIGIN);
-
-                return gitRepository;
-            }
-
         } catch (IOException e) {
-            log.error("Error while checking the status of {}", gitRepository.getName());
-            throw new AspenRestException(RestConstants.ERROR_WHILE_CHECKING_BRANCH_STATUS, Response.Status.ACCEPTED);
+            log.error("Error while building a git instance", e);
+            throw new AspenRestException(RestConstants.ERROR_BUILDING_GIT_INSTANCE, Response.Status.ACCEPTED);
         }
 
         log.debug(RestConstants.GIT_REPOSITORY_IS_UP_TO_DATE);
@@ -129,20 +132,14 @@ public class GitServiceImpl implements GitService {
         return gitRepository;
     }
 
-    private Git getGitInstance(String repositoryPath) {
+    private Repository getJGitRepository(String repositoryPath) throws IOException {
         // the .git directory inside the repository
         Path gitRepositoryConfigPath = Paths.get(repositoryPath, ".git");
-        FileRepositoryBuilder builder = new FileRepositoryBuilder();
-        Repository repository = null;
-        try {
-            repository = builder.setGitDir(gitRepositoryConfigPath.toFile())
-                    .readEnvironment() // scan environment GIT_* variables
-                    .build();
-        } catch (IOException e) {
-            log.error("Error while building a git instance", e);
-            throw new AspenRestException(RestConstants.ERROR_BUILDING_GIT_INSTANCE, Response.Status.ACCEPTED);
-        }
 
-        return new Git(repository);
+        FileRepositoryBuilder builder = new FileRepositoryBuilder();
+        return builder
+                .setGitDir(gitRepositoryConfigPath.toFile())
+                .readEnvironment() // scan environment GIT_* variables
+                .build();
     }
 }
