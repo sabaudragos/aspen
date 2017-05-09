01dcae0b9a0687b61e9da8abd6b075e810ded36b
dragos <sabaudragos@yahoo.com>
2017-05-09 20:52:40 +0300

    - fixed jersey issue, the app can now be started using java -jar aspen ...

--- src/main/java/xyz/codeark/JerseyConfig.java
+++ src/main/java/xyz/codeark/JerseyConfig.java
@@ -3,8 +3,13 @@ package xyz.codeark;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.glassfish.jersey.servlet.ServletProperties;
 import org.springframework.stereotype.Component;
+import xyz.codeark.rest.DirectoryDiscoveryResource;
+import xyz.codeark.rest.GitResource;
+import xyz.codeark.rest.MavenResource;
+import xyz.codeark.rest.RestConstants;
+import xyz.codeark.rest.exceptions.AspenRestException;
+import xyz.codeark.rest.exceptions.AspenRestExceptionMapper;
 
-import javax.ws.rs.ApplicationPath;
 
 /**
  * Allows static content to be displayed
@@ -13,7 +18,12 @@ import javax.ws.rs.ApplicationPath;
 public class JerseyConfig extends ResourceConfig {
 
     public JerseyConfig() {
-        packages("xyz.codeark.rest");
+        register(DirectoryDiscoveryResource.class);
+        register(GitResource.class);
+        register(MavenResource.class);
+        register(RestConstants.class);
+        register(AspenRestException.class);
+        register(AspenRestExceptionMapper.class);
         property(ServletProperties.FILTER_FORWARD_ON_404, true);
     }
 }
